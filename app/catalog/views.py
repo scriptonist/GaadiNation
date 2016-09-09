4775f7aa1175d287145df9350d47ecb2d8be89d5
scriptonist <aravind.techbridge@gmail.com>
2016-09-09 06:58:44 +0530

    Getting the catalog filters to work

--- app/catalog/views.py
+++ app/catalog/views.py
@@ -1,13 +1,20 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template,request,jsonify
 from app.models import filter_by_many_values_perfect
+import json
 
 catalog = Blueprint("catalog", __name__, url_prefix='/catalog')
 
 
-@catalog.route('/')
+@catalog.route('/',methods=["post","get"])
 def compare_index():
-    content = filter_by_many_values_perfect()
-    return render_template("catalog/catalog_index.html",result=content)
+    if request.method == "GET":
+        content = filter_by_many_values_perfect()
+        return render_template("catalog/catalog_index.html",result=content)
+    else:
+
+        print(filters)
+        content = filter_by_many_values_perfect()
+        return render_template("catalog/catalog_index.html", result=content)
 
 
 
--- app/static/js/catalog/getfilter.js
+++ app/static/js/catalog/getfilter.js
@@ -1,7 +1,23 @@
 $(document).ready(function(){
-    var filtert = {"cars":[]};
+    var filters = {"brand":[]};
+     function sendData() {
+            $.ajax({
+                url: "/catalog/",
+                type: 'POST',
+                 contentType: 'application/json',
+                data: { json: JSON.stringify(filters)
+                 },
+                dataType: 'json'
+            });
+     }
+
     $('input:checkbox').click(function(){
         var id = $(this).attr('id');
-        console.log(id);
+        if($(this).attr('class') === "brand"){
+            filters.brand.push(id);
+            console.log(filters);
+            sendData();
+        }
+
     });
 });
