3e58a41b19e1c0808bb10caa4c2627143198cbd0
scriptonist <aravind.techbridge@gmail.com>
2016-09-15 10:11:30 +0530

    refactored recomendation template

--- app/recomendation/views.py
+++ app/recomendation/views.py
@@ -1,14 +1,23 @@
-from flask import Blueprint,render_template,request
+from flask import Blueprint, render_template, request
 
-recomendation = Blueprint("recomendation", __name__, url_prefix='/recomendation')
+recomendation = Blueprint("recomendation", __name__,
+                          url_prefix='/recomendation')
 
-@recomendation.route('/questions')
+
+@recomendation.route('/', methods=['post', 'get'])
 def recomendation_steps():
     return render_template("recomendation/find.html")
 
-@recomendation.route('/result',methods=["POST","GET"])
+
+@recomendation.route('/result', methods=["POST", "GET"])
 def recomendation_result():
+    form = request.form
+    choices = {}
     if request.method == "POST":
-        return render_template("recomendation/findresult.html",form=request.form)
+        for key in form.keys():
+            k = key.split(":")
+            choices[k[0]] = k[1]
+        print(choices)
+        return render_template("recomendation/findresult.html", form=request.form)
     else:
         return render_template('404.html')
