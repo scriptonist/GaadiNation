695140b734520d668c8c21f3f9ade14f736d28f9
scriptonist <aravind.techbridge@gmail.com>
2016-09-15 15:42:37 +0530

    refactored some code in recomendation module

--- app/recomendation/views.py
+++ app/recomendation/views.py
@@ -1,12 +1,17 @@
 from flask import Blueprint, render_template, request
+from app.models import filter_by_many_values_perfect
 
 recomendation = Blueprint("recomendation", __name__,
                           url_prefix='/recomendation')
 
-
+"""
+    This module recomends a car based on a users preferences
+    if the following values are choosen for the preferences he 
+    choose.
+"""
 budget = {"cheap": 1000000, "affordable": 3000000, "luxury": 3000000}
 seatnumber = [2, 4, 5, 6, 7, 8]
-use = {"daily": 20, "weekly": 15, "occasionaly": 10}
+use = {"daily": 18, "weekly": 15, "occasionaly": 10}
 
 
 @recomendation.route('/', methods=['post', 'get'])
@@ -16,13 +21,30 @@ def recomendation_steps():
 
 @recomendation.route('/result', methods=["POST", "GET"])
 def recomendation_result():
+    """
+        This function will filter out cars from the db
+        according to the preferences of the user
+    """
     form = request.form
     choices = {}
+    conditions = []
     if request.method == "POST":
         for key in form.keys():
             k = key.split(":")
             choices[k[0]] = k[1]
-        print(choices)
-        return render_template("recomendation/findresult.html", form=request.form)
+        if choices['budget'] == 'luxury':
+            conditions.append({"start_price": {'$gte': budget['luxury']}})
+            conditions.append({"SeatCapacity": {'$gte': seatnumber[seatnumber.index(int(choices['seatnumber']))]}})
+        elif choices['budget'] == 'cheap':
+            conditions.append({"start_price": {'$lte': budget['cheap']}})
+            conditions.append({"SeatCapacity": {'$gte': seatnumber[seatnumber.index(int(choices['seatnumber']))]}})
+            conditions.append({"Mileage": {'$gte': use[choices['use']]}})
+        elif choices['budget'] == 'affordable':
+            conditions.append({"start_price": {'$lte': budget['affordable']}})
+            conditions.append({"start_price": {'$gt': budget['cheap']}})
+            conditions.append({"SeatCapacity": {'$gte': seatnumber[seatnumber.index(int(choices['seatnumber']))]}})
+            conditions.append({"Mileage": {'$gte': use[choices['use']]}})
+        content=filter_by_many_values_perfect(conditions)
+        return render_template("recomendation/findresult.html", content=content)
     else:
         return render_template('404.html')
