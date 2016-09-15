ae6ba76b596af7498fb1bd36d66963d236556a4f
scriptonist <aravind.techbridge@gmail.com>
2016-09-15 22:26:50 +0530

    Added add to compare in search results page

--- app/compare/views.py
+++ app/compare/views.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request
-from app.models import getcars, getcardetails,getmanufacturer
+from app.models import getcars, getcardetails, getmanufacturer
 import json
 
 compare = Blueprint("compare", __name__, url_prefix='/compare')
@@ -7,32 +7,51 @@ compare = Blueprint("compare", __name__, url_prefix='/compare')
 
 @compare.route('/')
 def compare_index():
-    return render_template("compare/compare_index.html",manu=getmanufacturer())
-
+    return render_template("compare/compare_index.html", manu=getmanufacturer())
 
 
 @compare.route('/result/<brand>', methods=['GET'])
 def carlist(brand):
-    cars=getcars(brand)
+    cars = getcars(brand)
     return json.dumps(cars)
+
 
 @compare.route('/compare_display', methods=['POST'])
 def compare_display():
-    number=count=0
-    details1=getcardetails(request.form['list1'])
-    details2=getcardetails(request.form['list2'])
-    details3=getcardetails(request.form['list3'])
-    details4=getcardetails(request.form['list4'])
-    cars=[details1,details2,details3,details4]
+    number = count = 0
+    details1 = getcardetails(request.form['list1'])
+    details2 = getcardetails(request.form['list2'])
+    details3 = getcardetails(request.form['list3'])
+    details4 = getcardetails(request.form['list4'])
+    cars = [details1, details2, details3, details4]
     for k in cars:
-        if len(k)>0:
-            count=count+1
-    if count==1 :
-       number=37
-    elif count==2 :
-        number=17
-    elif count==3 :
-        number=6
-    else :
-        number=0
-    return render_template("compare/compare_result1.html",cars=cars,number=number)
+        if len(k) > 0:
+            count = count + 1
+    if count == 1:
+        number = 37
+    elif count == 2:
+        number = 17
+    elif count == 3:
+        number = 6
+    else:
+        number = 0
+    return render_template("compare/compare_result1.html", cars=cars, number=number)
+
+
+@compare.route('/compare_display/<carlist>', methods=['GET'])
+def compare_display2(carlist=None):
+    carlist = carlist.split(",")
+    print(carlist)
+    details = []
+    for car in carlist:
+        details.append(getcardetails(car))
+    count = len(carlist)
+    if count == 1:
+        number = 37
+    elif count == 2:
+        number = 17
+    elif count == 3:
+        number = 6
+    else:
+        number = 0
+    return render_template("compare/compare_result1.html",cars=details,number=number)
