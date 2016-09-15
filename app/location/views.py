e8bf33602804c46552657e9480443921ed8c0a39
scriptonist <aravind.techbridge@gmail.com>
2016-09-15 08:25:48 +0530

    completed the store locator module

--- app/location/views.py
+++ app/location/views.py
@@ -1,14 +1,40 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template,request
+from urllib.request import urlopen
+from urllib.parse import urlencode
+import json
 
-location = Blueprint("location", __name__, url_prefix='/location')
+location = Blueprint("location", __name__, url_prefix='/locate')
+
+
+googleGeocodeUrl = 'http://maps.googleapis.com/maps/api/geocode/json?'
+
+def get_coordinates(query, from_sensor=False):
+    query = query.encode('utf-8')
+    params = {
+        'address': query,
+        'sensor': "true" if from_sensor else "false"
+    }
+    url = googleGeocodeUrl + urlencode(params)
+    json_response = urlopen(url)
+    response = json.loads(json_response.read().decode('utf-8'))
+    if response['results']:
+        loc = response['results'][0]['geometry']['location']
+        latitude, longitude = loc['lat'], loc['lng']
+    else:
+        latitude, longitude = None, None
+    return latitude, longitude
 
 
 @location.route('/',methods=["post","get"])
 def locate_index():
-    return render_template('location/sharelocation.html')
-@location.route('/<int:lati>/<int:longi>',methods=["post","get"])
-def locate():
-    return render_template('location/location.html',lat=lati,lon=longi)
+    if request.method == "GET":
+        return render_template('location/location.html',lat=9.5916,lon=76.5222)
+    else:
+        query = request.form['searchTextField']
+        l = get_coordinates(query)
+        return render_template('location/location.html',lat=l[0],lon=l[1])
+
+
 
 
 
